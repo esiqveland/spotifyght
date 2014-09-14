4bc9190099c503c86896a59074fc30a05f17d2fa
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-09-14 13:29:54 +0200

    cors

--- routes/cors.js
+++ routes/cors.js
@@ -2,14 +2,17 @@
  * CORS support.
  */
 
+// use "*" here to accept any origin
+var Allow_Origin = 'http://localhost:3000 *.spotilocal.com';
 
 exports.setup = function (req, res, next) {
   if (!req.get('Origin')) return next();
-  // use "*" here to accept any origin
-  res.set('Access-Control-Allow-Origin', 'http://localhost:3000 *.spotilocal.com');
+  res.set('Access-Control-Allow-Origin', Allow_Origin);
   res.set('Access-Control-Allow-Methods', 'GET PUT POST DELETE');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
   // res.set('Access-Control-Allow-Max-Age', 3600);
-  if ('OPTIONS' == req.method) return res.send(200);
+  if ('OPTIONS' == req.method) {
+    return res.send(200);
+  }
   next();
 };
