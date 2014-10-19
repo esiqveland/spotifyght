566307a01f95659b1cbaf235e05e3cbb198b31a1
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-10-19 15:08:04 +0200

    generify docker.sh

--- docker.sh
+++ docker.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+PROJECT=esiqveland/spotifyghtapi
 
 case "$1" in
-	build) docker build -t "esiqveland/spotifyghtapi" .
+	build) docker build -t "$PROJECT" .
 		;;
-	run) docker run --name spotifyghtapi --link my-redis:redis -p 127.0.0.1:3001:3001 -d  esiqveland/spotifyghtapi
+	run) docker run --name spotifyghtapi --link my-redis:redis -p 127.0.0.1:3001:3001 -d $PROJECT
+		;;
+	pull) docker pull $PROJECT
+		;;
+	push) docker push $PROJECT
 		;;
 	*) echo "Usage: $0 [build|run]"
 		;;
