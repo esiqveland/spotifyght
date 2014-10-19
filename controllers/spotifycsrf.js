ef9ebb7d799e7f2daad8a55852108c090a87f13b
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-10-20 00:47:09 +0200

    spotifycsrf: refactor

--- controllers/spotifycsrf.js
+++ controllers/spotifycsrf.js
@@ -3,10 +3,19 @@ var request = require('request');
 var oauthTokenUrl = 'https://open.spotify.com';
 var oauthTokenPath = '/token';
 
-exports.oauthTokenUrl = oauthTokenUrl;
-exports.oauthTokenPath = oauthTokenPath;
+var spotify_csrf_proxy = function(req, res, next) {
+  getToken(function(err, body) {
+    if(err) {
+      next(new Error(error));
+      return;
+    }
+    var obj = JSON.parse(body);
+    res.status(200);
+    res.send(obj);
+  });
+};
 
-var getToken = function(callback) {
+function getToken(callback) {
   request(oauthTokenUrl+oauthTokenPath, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body);
@@ -18,14 +27,7 @@ var getToken = function(callback) {
   })
 };
 
-exports.spotify_csrf_proxy = function(req, res, next) {
-  getToken(function(err, body) {
-    if(err) {
-      next(new Error(error));
-      return;
-    }
-    var obj = JSON.parse(body);
-    res.status(200);
-    res.send(obj);
-  });
-};
+
+exports.oauthTokenUrl = oauthTokenUrl;
+exports.oauthTokenPath = oauthTokenPath;
+exports.spotify_csrf_proxy = spotify_csrf_proxy;
