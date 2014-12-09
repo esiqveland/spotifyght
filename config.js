7a53f3f356d6e25021f02d63e632be7dfc642da6
Eivind Siqveland Larsen <to.eivind@gmail.com>
2014-12-10 00:36:47 +0100

    config redis password

--- config.js
+++ config.js
@@ -4,8 +4,9 @@ var config = {
     env: process.env.ENVIRONMENT  || 'development',
     redis: {
       //host: '10.0.1.100',
-      host: 'redis',
-      port: 6379
+      host: 'redis',      
+      port: 6379,
+      password: process.env.REDIS_PASS || undefined
     }
 };
 
